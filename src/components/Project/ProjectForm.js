0730feb4779219645844ee2215f1400751b32ae5
cleyton21 <cfernando_21@hotmail.com>
2024-12-18 20:17:34 -0300

    Conectando com API pelo React

--- src/components/Project/ProjectForm.js
+++ src/components/Project/ProjectForm.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import styles from './ProjectForm.module.css';
 
 import Input from '../form/Input';
@@ -5,6 +7,23 @@ import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
 
 function ProjectForm({ btnText }) {
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setCategories(data);
+            })
+            .catch((err) => console.log(err));
+    }, []);
+
     return (
         <form className={styles.form}>
             <Input 
@@ -19,7 +38,11 @@ function ProjectForm({ btnText }) {
                 name="budget" 
                 placeholder="Insira o orçamento total" 
             />
-           <Select name="category_id" text="Selecione a categoria"/>
+           <Select 
+            name="category_id" 
+            text="Selecione a categoria"
+            options={categories}
+            />
             
             <SubmitButton text={btnText} />
         </form>
