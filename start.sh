76ff2345a763cdb299aa48873679d2fbb2eee158
cleyton21 <cfernando_21@hotmail.com>
2024-12-22 19:40:52 -0300

    deploy7

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ echo "Iniciando nginx..."
 nginx
 
 echo "Iniciando json-server na porta 5000..."
-cd /app && json-server --watch db.json --port 5000 --host 0.0.0.0 --no-cors
+cd /app && json-server --watch db.json --port 5000 --host 0.0.0.0
